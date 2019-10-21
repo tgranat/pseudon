c20850236e1fb80d0ada66d046d4dc0d44803189
Torbjorn Granat <torbjorn.granat@gmail.com>
2019-10-21 17:05:38 +0200

    Add test data generation

---
+++ pseudon/__main__.py
@@ -0,0 +1,47 @@
+import sys
+import getopt
+
+from pseudon.util import gen_test_data 
+
+def main():
+    print("main() in __main__.py %s", __name__)
+    
+    UNIXOPTS = 'hg:r:'
+    GNUOPTS = ['help', 'gen_test_data=', 'rows=']
+    HELP = '[-g file -r rows -h]' +\
+    '\n-g file\tGenerate test data file' +\
+    '\n-r rows\tNumber of rows in test data file' +\
+    '\n-h\tHelp'
+            
+    argv = sys.argv[1:]
+    gen_data_file = ''
+    gen_data_file_rows = 0
+    
+    try:
+        opts, args = getopt.getopt(argv, UNIXOPTS, GNUOPTS)
+        print(opts)
+        print(args)
+        for opt, value in opts:
+            if opt in ('-h', '--help'):
+                print('usage: %s' % sys.argv[0], HELP)
+            if opt in ('-g', '--gen_test_data'):
+                gen_data_file = value
+            if opt in ('-r', 'rows'):
+                gen_data_file_rows = int(value)
+                
+                
+    except (getopt.GetoptError, ValueError) as err:
+        print(err)  
+        print('usage: %s' % sys.argv[0], HELP)     
+        sys.exit(2)
+
+# Generate test data file
+    if gen_data_file:
+        if gen_data_file_rows <= 0:
+            gen_test_data(gen_data_file)
+        else:
+            gen_test_data(gen_data_file, gen_data_file_rows)
+
+
+if __name__ == "__main__":
+    main()
