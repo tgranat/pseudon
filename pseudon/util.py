cdc6e0d06e4fa05e6e5d72304dfdc97f6ab1ae2b
Torbjorn Granat <torbjorn.granat@gmail.com>
2019-10-21 14:36:17 +0200

    Function to generate test data

--- pseudon/util.py
+++ pseudon/util.py
@@ -1,7 +1,6 @@
-
-# create new key from key + tweak (salt)
-
 import secrets
+import csv
+import random
 
 def generate_token_bytes(nbytes = 16):
     return secrets.token_bytes(nbytes)
@@ -11,3 +10,29 @@ def generate_token_hex(nbytes = 16):
 
 def generate_token_urlsafe(nbytes = 16):
     return secrets.token_urlsafe(nbytes)
+
+def generate_random_ascii(length = 10):
+    #letters = string.ascii_lowercase
+    letters = 'abcdefghijklmnoprstuv'
+    # using list comprehension: 
+    return ( ''.join(random.choice(letters) for i in range(length)))
+
+    
+# Generate test data: 
+# CSV file with columns: full name, email, phone
+# phone unique
+
+def gen_test_data(filename, rows=100, header=False):
+    phonelist = random.sample(range(100000000,999999999), rows)
+    with open (filename, 'w', newline='') as outfile:
+        csv_file_writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        if header:
+            csv_file_writer.writerow(['Full name', 'Email', 'Phone'])
+        for phone in phonelist:
+            firstname = generate_random_ascii(random.randint(3,5))
+            lastname = generate_random_ascii(random.randint(5,8))
+            
+            csv_file_writer.writerow([firstname.capitalize() + \
+                ' ' + lastname.capitalize(), lastname + '@example.com', '0' + str(phone)])
+
+
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,20 +1,51 @@
-from pseudon.util import generate_token_bytes, generate_token_hex, generate_token_urlsafe
+from pseudon.util import \
+generate_token_bytes, \
+generate_token_hex,\
+generate_token_urlsafe,\
+gen_test_data,\
+generate_random_ascii
 
-def test_generate_token_bytes():
-    bytes_token = generate_token_bytes()
-    assert(type(bytes_token) == bytes)
-    assert(len(bytes_token) == 16)
+class TestToken():
+    def test_generate_token_bytes(self):
+        bytes_token = generate_token_bytes()
+        assert(type(bytes_token) == bytes)
+        assert(len(bytes_token) == 16)
     
-def test_generate_token_hex():
-    length = 8
-    hex_token = generate_token_hex(length)
-    assert(type(hex_token) == str)
-    assert(len(hex_token) == length*2)
+    def test_generate_token_hex(self):
+        length = 8
+        hex_token = generate_token_hex(length)
+        assert(type(hex_token) == str)
+        assert(len(hex_token) == length*2)
     
-def test_generate_token_urlsafe():
-    urlsafe_token = generate_token_urlsafe(32)
-    assert(type(urlsafe_token) == str)
-    # can't test length since each byte results in approximately 1.3 characters
-    # can decode and encode and compare perhaps
+    def test_generate_token_urlsafe(self):
+        urlsafe_token = generate_token_urlsafe(32)
+        assert(type(urlsafe_token) == str)
+        # can't test length since each byte results in approximately 1.3 characters
+        # can decode and encode and compare perhaps
     
     
+class TestTestData():
+    def test_gen_data(self, tmpdir):
+        rows  = 1000
+        outfile = tmpdir.mkdir("test_gen_data").join("testdata.csv")
+        gen_test_data(outfile, rows)
+        x = 0
+        with open (outfile, 'r') as f:
+            for line in f:
+                print(line)
+                x += 1
+        assert(x == rows)
+  
+    def test_gen_data_header(self, tmpdir):
+        rows = 10
+        outfile = tmpdir.mkdir("test_gen_data").join("testdata1.csv")
+        gen_test_data(outfile, rows, header=True)
+        x = 0
+        with open (outfile, 'r') as f:
+            for line in f:
+                x += 1
+        assert(x == rows + 1)      
+        
+    def test_gen_ascii(self):
+        assert(len(generate_random_ascii(7)) == 7)
+        
