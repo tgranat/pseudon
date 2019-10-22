dd06772b029c5368b74fe0a5491916aa48e04221
Torbjorn Granat <torbjorn.granat@gmail.com>
2019-10-22 16:00:26 +0200

    encrypt numeric method

--- pseudon/pseudonymizer.py
+++ pseudon/pseudonymizer.py
@@ -1,35 +1,38 @@
 import logging
+import pyffx
 
-from .util import generate_token_urlsafe
+from .util import generate_token_bytes
 
 logger = logging.getLogger(__name__)
 
 # Pseudonymize data in a CSV file
 
 class Pseudonymizer():
-    logging.info('Create Pseudonymizer')
     
-    #secret_key_base64 = ''
-    
-    def __init__(self, secret_key_base64 = ''):
-        if secret_key_base64:
-            self.secret_key_base64 = secret_key_base64
+    def __init__(self, secret_key_hex = ''):
+        logging.info('Init Pseudonymizer')
+        if secret_key_hex:
+            self.secret_key_bytes = bytes.fromhex(secret_key_hex)
+            logging.debug('Use secret key received: {}'.format(secret_key_hex))
         else:
-            self.secret_key_base64 = generate_token_urlsafe(16)
-            
+            self.secret_key_bytes = generate_token_bytes(16)
+            logging.debug('Generated secret key: {}'.format(self.secret_key_bytes))
 
-    def get_secret_key(self):
-        return self.secret_key_base64
 # Local:
 # In: CSV row (a list)
 # Out: updates CSV row (a list)
 
-    def update_row(self, row):
-        return
+    def update_row(self, column_num, column_type, row):
+        if column_type == 'num':
+            logging.debug('Pseudonymize {}'.format(row[column_num-1]))
+            encrypter = pyffx.Integer(self.secret_key_bytes, len(row[column_num-1]))
+            row[column_num-1] = encrypter.encrypt(row[column_num-1])
+        return row
    
 # Run job:
 #  specify file info:
 #  column_num, column_type, header_present=False,
+# column_num starts with 1 (first column)
 #  column type can be: num, asc, (email, full name etc. maybe not implement)
 #  In and out files
     def run_job(self, infile, outfile, column_num=1, column_type='num', header_present=False):
--- tests/test_pseudonymizer.py
+++ tests/test_pseudonymizer.py
@@ -1,6 +1,23 @@
 from pseudon.pseudonymizer import Pseudonymizer
+from pseudon.util import generate_token_hex
+
+#import pyffx
 
 def test_create_pseudonymizer():
-    p = Pseudonymizer('qwerty')
-    assert(p.get_secret_key == 'qwerty')
+    h = generate_token_hex(4)
+    q = Pseudonymizer(h)
+    assert(q.secret_key_bytes.hex() == h)
     
+    
+def test_update_row_num():
+    p = Pseudonymizer()
+    row = ['abcde', '12345678', 'cdefg']
+    result_row = p.update_row(2, 'num', row)
+    assert(result_row[0] == 'abcde')
+    assert(result_row[1] != '123455678')
+    assert(result_row[2] == 'cdefg')
+    
+#def test_pyffx():
+#    p = Pseudonymizer()
+#    e = pyffx.Integer(p.secret_key_bytes, 4)
+#    print('encrypted: {:d}'.format(e.encrypt('1234')))
